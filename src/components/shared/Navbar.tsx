391c677ebdffef0961f144788a17bd8139669ad0
alamshuvo <iftakharalamshuvo11@gmail.com>
2025-03-22 00:39:10 +0600

    dropdown added

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,6 +1,16 @@
 import Logo from "@/app/assets/svgs/Logo";
 import { Button } from "../ui/button";
-import { Heart, ShoppingBag } from "lucide-react";
+import { Heart, LogOut, ShoppingBag } from "lucide-react";
+import Link from "next/link";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Navbar() {
   return (
@@ -24,6 +34,37 @@ export default function Navbar() {
           <Button variant="outline" className="rounded-full p-0 size-10">
             <ShoppingBag />
           </Button>
+          <Link href={"/login"}>
+            <Button variant={"outline"} className="rounded-full">
+              Login
+            </Button>
+          </Link>
+          <Link href={"/create-shop"}>
+            <Button variant={"outline"} className="rounded-full">
+              Create Shop
+            </Button>
+          </Link>
+
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Dashboard</DropdownMenuItem>
+              <DropdownMenuItem>My shop</DropdownMenuItem>
+              <DropdownMenuSeparator></DropdownMenuSeparator>
+              <DropdownMenuItem>
+                <LogOut></LogOut>
+                <span>Logout</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </nav>
       </div>
     </header>
